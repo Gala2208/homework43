40fa1c16433e38a62851f7e23ac6c18c1b12aa50
Gala2208 <toppers.ogre-0k@icloud.com>
2022-11-18 13:03:04 +0500

    Create

---
+++ homework43/Program.cs
@@ -0,0 +1,7 @@
+﻿double b1 = Convert.ToDouble(Console.ReadLine()),
+       k1 = Convert.ToDouble(Console.ReadLine()),
+       b2 = Convert.ToDouble(Console.ReadLine()),
+       k2 = Convert.ToDouble(Console.ReadLine()),
+        x = (b2 - b1) / (k1 - k2),
+        y = (k1 * ((b2 - b1) / (k1 - k2))) + b1;
+Console.WriteLine($"{x}, {y}");
